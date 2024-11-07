f9827b321a378b6a1d27d341baf89c721099abc1
GuiibScripts <74783440+GuiibScripts@users.noreply.github.com>
2024-11-07 09:47:30 -0300

    Update Chaves_Cadastradas.lua

--- Chaves_Cadastradas.lua
+++ Chaves_Cadastradas.lua
@@ -11,14 +11,7 @@
                 local AntiGM = false  
                 
                 -- Função para carregar o nome
-                function carregarnome()
-                    local playername = Player.getName() 
-                    
-                    Palavras_Chave[1] = playername
-                end
-                
-                -- Chama a função para carregar o nome
-                carregarnome()
+
                 
                 
                 local ANTIGMTEXT = HUD.new(15, 35, "Anti GM: OFF", true); ANTIGMTEXT:setFontSize(11);
